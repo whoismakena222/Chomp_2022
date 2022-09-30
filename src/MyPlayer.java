141196e23ad5bca32feab8f8ed3f42c787c5d088
killiam-cs <charlotte_killiam@milton.edu>
2022-09-30 09:24:15 -0400

    Initial commit, includes graphics changes by Charlotte Killiam for the 2022-2023 school year.

---
+++ src/MyPlayer.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+
+// this class runs when the "MyPlayer" button is pressed
+// this is where students will write their own code
+
+public class MyPlayer extends Player{
+    public Chip[][] gameBoard;
+    public int[] columns;
+
+    public MyPlayer() {
+        columns = new int[10];
+
+        /***
+         * This code will run just once, when the game opens.
+         */
+    }
+
+    public Point move(Chip[][] pBoard) {
+        System.out.println("MyPlayer");
+
+        gameBoard = pBoard;
+        int column = 0;
+        int row = 0;
+
+        row = 1;
+        column = 1;
+
+        /***
+         * This code will run each time the "MyPlayer" button is pressed.
+         * Add your code to return the row and the column of the chip you want to take.
+         * You'll be returning a data type called Point which consists of two integers.
+         */
+
+        Point myMove = new Point(row, column);
+        return myMove;
+    }
+
+}
