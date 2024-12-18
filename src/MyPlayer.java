e44240b4b9dea7c965cfcacd499027a54f577689
whoismakena222 <makena_ndiritu25@milton.edu>
2024-12-18 09:06:53 -0500

    12/18

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -3,14 +3,25 @@ import java.awt.*;
 public class MyPlayer {
     public Chip[][] gameBoard;
     public int[] columns;
+    public int[] rows;
 
     public MyPlayer() {
         columns = new int[10];
+        rows = new int[10];
 
         /***
          * This code will run just once, when the game opens.
          * Add your code here.
          */
+
+        for(int i = 0; i < columns.length; i++){
+            columns[i] = 0;
+            for(int j = 0; j < rows.length; i++){
+                if (gameBoard[i][j].isAlive == true){
+                    System.out.println("chip at row: " + i + "column: " + j);
+                }
+            }
+        }
     }
 
     public Point move(Chip[][] pBoard) {
@@ -29,6 +40,13 @@ public class MyPlayer {
          * Add your code to return the row and the column of the chip you want to take.
          * You'll be returning a data type called Point which consists of two integers.
          */
+
+        // add code to fill in the columns array
+        // so that it represents the gameBoard in number form
+        // print your columns array to show its working
+        System.out.println("MY PLAYER CLICKED");
+
+
 
         Point myMove = new Point(row, column);
         return myMove;
